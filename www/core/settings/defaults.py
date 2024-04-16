8e8c3dc2497e39e97902f9ad40f7cab357bab2ee
a-mt <amistri@live.fr>
2024-04-16 21:19:13 +0200

    Test CI/CD

--- www/core/settings/defaults.py
+++ www/core/settings/defaults.py
@@ -201,3 +201,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# ---------------------------------------------------------
+# Others
+# ---------------------------------------------------------
+
+COMMIT_SHA = os.getenv('COMMIT_SHA', 'latest')
+COMMIT_TAG = os.getenv('COMMIT_TAG', '') or COMMIT_SHA
--- www/core/views.py
+++ www/core/views.py
@@ -9,5 +9,7 @@ def about(request):
     data = json.dumps({
         'env': 'prod' if 'PRODUCTION' in os.environ else 'dev',
         'debug': settings.DEBUG,
+        'commit-sha': settings.COMMIT_SHA,
+        'commit-tag': settings.COMMIT_TAG,
     })
     return HttpResponse(data, content_type='application/json')
